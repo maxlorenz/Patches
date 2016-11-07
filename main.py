343095f69b05e029842fad91c171da355aa3442e
Max Lorenz <max-lorenz@gmx.net>
2016-11-07 14:27:27 +0100

    Added percentage to run script

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import field
-
 b = field.Board(47, 5)
-print b.get_cost()
 
-for i in xrange(10000):
+for i in xrange(100000):
     current_val = b.get_cost()
     b.swap_random()
 
     if b.get_cost() <= current_val:
         b.unswap()
     
-    if i % 100 == 0:
-        print i, ": ", current_val
-
-print "Result: ", current_val
+    if i % 1000 == 0:
+        print i/1000, "%:", current_val
 
 for patch in [ patch for row in b.patches for patch in row ]:
-    print "Number: ", patch.number, "Position: ", patch.position
+    row = patch.position['y'] + 1
+    column = patch.position['x'] + 1
+
+    print "Number:", patch.number, "Row:", row, "Column:", column
